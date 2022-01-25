5960c47ff4662f00635807b4d521514d7ff99a5a
Sajal <sajal2526gupta@gmail.com>
2022-01-25 16:07:50 +0530

    logic build sucessfully

--- FCFS (First Come First Serve) CPU Scheduling Algorithm with different Arrival Time.cpp
+++ FCFS (First Come First Serve) CPU Scheduling Algorithm with different Arrival Time.cpp
@@ -7,25 +7,47 @@ using namespace std;
 
 
 int main() {
-    int A[4],B[4],C[4],wt=0;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int process[4];
+    int at[4];
+    int bt[4];
     for(int i=0;i<4;i++){
-        cin>>A[i]>>B[i]>>C[i];
+        cin>>process[i]>>at[i]>>bt[i];
     }
-    for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++){
-            if(B[j]==i+1){
-                if(i==0)
-                A[j]=wt;
-                else
-                    A[j]=wt-B[j];
-                wt+=C[j];
+      for(int i=0;i<3;i++){
+        for(int j=i+1;j<4;j++){
+            if(at[j]<at[i]){
+                int temp=at[j];
+                at[j]=at[i];
+                at[i]=temp;
+                
+                temp=process[j];
+                process[j]=process[i];
+                process[i]=temp;
+                
+                temp=bt[j];
+                bt[j]=bt[i];
+                bt[i]=temp;
             }
         }
     }
-    for(int i=0;i<4;i++)
-        if(i!=3)
-        cout<<"P"<<i+1<<" (WT="<<A[i]<<"), ";
-    else
-        cout<<"P"<<i+1<<" (WT="<<A[i]<<")";
+    int x=0;
+    int wt[4];
+    for(int i=0;i<4;i++){
+        if(i==0){
+            wt[process[i]-1]=0;
+        }
+        else{
+            wt[process[i]-1]=x-at[i];
+        }
+        x+=bt[i];
+}
+      for(int i=0;i<3;i++)
+    {
+        cout<<"P"<<i+1<<" "<<"(WT="<<wt[i]<<"), ";
+    }
+    cout<<"P"<<4<<" "<<"(WT="<<wt[3]<<")";
+    
+    
     return 0;
 }
